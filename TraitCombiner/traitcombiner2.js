e8bffc5fc06b41fe94095070ed57afe1360f8d57
thebobfather2 <107475188+thebobfather2@users.noreply.github.com>
2023-01-19 21:42:24 -0500

    WORKING
    Signed-off-by: thebobfather2 <107475188+thebobfather2@users.noreply.github.com>

--- TraitCombiner/traitcombiner2.js
+++ TraitCombiner/traitcombiner2.js
@@ -1,9 +1,12 @@
 var slct1 = document.querySelector('#slct1');
 slct1.options[slct1.selectedIndex].id;
 
+var slct2 = document.querySelector('#slct2');
+slct2.options[slct2.selectedIndex].id;
+
     let imgEle1 = "./img/" + slct1.value +'.png';
-    let imgEle2 = document.querySelectorAll(".image")[1];
-    let imgEle3 = document.querySelectorAll(".image")[2];
+    let imgEle2 = "./img/" + slct2.value +'.png';
+    let imgEle3 = "./img/" + slct3.value +'.png';
     let imgEle4 = document.querySelectorAll(".image")[3];
     let imgEle5 = document.querySelectorAll(".image")[4];
     let resEle = document.querySelector(".result");
@@ -26,44 +29,25 @@ slct1.options[slct1.selectedIndex].id;
 
 // round 3 output test
 
-function getBackground() {
-  selectElement = document.querySelector('#slct1');
-  output = selectElement.value;
-  document.querySelector('.output').textContent = output;
-}
+// function getBackground() {
+//   selectElement = document.querySelector('#slct1');
+//   output = selectElement.value;
+//   document.querySelector('.output').textContent = output;
+// }
 
-function getSkin() {
-  selectElement = document.querySelector('#slct2');
-  output = selectElement.value;
-  document.querySelector('.output').textContent = output;
-}
+// function getSkin() {
+//   selectElement = document.querySelector('#slct2');
+//   output = selectElement.value;
+//   document.querySelector('.output').textContent = output;
+// }
 
-// Create a JavaScript function that returns the file path that you want to display
-function getImageUrl() {
-  return "img/DesertSand.png";
-}
-
-// Use JavaScript to access the image element by its ID, and set its "src" attribute to the URL or file path returned by the function
-var img = document.getElementById("myImage");
-img.src = getImageUrl();
-
-// gety image first try
-document.getElementById('btn').onclick = function() {
-  var val = document.getElementById('imagename').value,
-      src = "./img/" + val +'.png',
-      img = document.createElement('img');
-      img.src = src;
-img.setAttribute('width', '190px');
-img.setAttribute('height', '190px');
-      document.body.appendChild(img);
-}
 // get image from dropdown:
-const select = document.getElementById('image-category');
-const option = select.options[select.selectedIndex];
-const val = document.getElementById('imagename').value;
-const images = option.value == '8' ? 'Images' : 'images';
+// const select = document.getElementById('image-category');
+// const option = select.options[select.selectedIndex];
+// const val = document.getElementById('imagename').value;
+// const images = option.value == '8' ? 'Images' : 'images';
 
-const path = `www.example.com/Category-${option.value}/${images}/{$val}.png`;
+// const path = `www.example.com/Category-${option.value}/${images}/{$val}.png`;
 
 //token gate what is available in dropdown:
 function populate (s1,s2) {
@@ -83,3 +67,85 @@ function populate (s1,s2) {
     s2.options.add(newOption);
   }
 }
+
+// create filepath for Background
+function createFilePathbg() {
+  var select = document.getElementById("slct1");
+  var selectedValue = select.options[select.selectedIndex].value;
+  var filePathbg = "img/Background/" + selectedValue + ".png";
+  return filePathbg;
+}
+// update image src for background
+var select = document.getElementById("slct1");
+select.addEventListener("change", function() {
+    var filePathbg = createFilePathbg();
+    var imgEle1 = document.getElementById("myImagebg");
+    imgEle1.src = filePathbg
+});
+
+// create filepath for Skin
+function createFilePathskin() {
+  var select2 = document.getElementById("slct2");
+  var selectedValue2 = select2.options[select2.selectedIndex].value;
+  var filePathskin = "img/Body/" + selectedValue2 + ".png";
+  return filePathskin;
+}
+// update image src for skin
+var select2 = document.getElementById("slct2");
+select2.addEventListener("change", function() {
+    var filePathskin = createFilePathskin();
+    var imgEle2 = document.getElementById("myImageskin");
+    imgEle2.src = filePathskin
+});
+
+// create filepath for eyes
+function createFilePatheyes() {
+  var select3 = document.getElementById("slct3");
+  var selectedValue3 = select3.options[select3.selectedIndex].value;
+  var filePatheyes = "img/Eyes/" + selectedValue3 + ".png";
+  return filePatheyes;
+}
+// update image src for eyes
+var select3 = document.getElementById("slct3");
+select3.addEventListener("change", function() {
+    var filePatheyes = createFilePatheyes();
+    var imgEle3 = document.getElementById("myImageeyes");
+    imgEle3.src = filePatheyes
+});
+
+// create filepath for mouth
+function createFilePathmouth() {
+  var select4 = document.getElementById("slct4");
+  var selectedValue4 = select4.options[select4.selectedIndex].value;
+  var filePathmouth = "img/Mouth/" + selectedValue4 + ".png";
+  return filePathmouth;
+}
+// update image src for mouth
+var select4 = document.getElementById("slct4");
+select4.addEventListener("change", function() {
+    var filePathmouth = createFilePathmouth();
+    var imgEle4 = document.getElementById("myImagemouth");
+    imgEle4.src = filePathmouth
+});
+
+// Create a JavaScript function that returns the file path that you want to display
+// function getImageUrl() {
+//   return "img/DesertSand.png";
+// }
+
+// // Use JavaScript to access the image element by its ID, and set its "src" attribute to the URL or file path returned by the function
+// var img = document.getElementById("myImage");
+// img.src = getImageUrl();
+
+// get image first try
+// document.getElementById('btn').onclick = function() {
+//   var val = document.getElementById('imagename').value,
+//       src = "./img/" + val +'.png',
+//       img = document.createElement('img');
+//       img.src = src;
+// img.setAttribute('width', '190px');
+// img.setAttribute('height', '190px');
+//       document.body.appendChild(img);
+// }
+
+console.log(imgEle1.value)
